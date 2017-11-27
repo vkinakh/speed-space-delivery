36660a879048c2391521ac6933a1cd4bce200ea2
andzav <andzav@users.noreply.github.com>
2017-11-27 23:10:32 +0200

    Update logout.js

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,10 +1,11 @@
 $("#logout").click(function (){
-	localStorage.clear();
+	
 	location.replace("index.html");
 	var url="https://someleltest.herokuapp.com/api/users/logout";
 	$.post(url,{
 		"SID":JSON.parse(localStorage.getItem("SID"))
 		},function (status){
+			localStorage.clear();
 			console.log(status);
 		});
 })
