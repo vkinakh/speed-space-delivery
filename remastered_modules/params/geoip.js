784316cc940034b9050b0cf4d611ad86f101b1b7
justOleh <olehvenhryniu@gmail.com>
2018-04-05 00:31:43 +0300

    commit

--- remastered_modules/params/geoip.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(next) {
-  let ip = this.req.headers['x-forwarded-for'] || this.req.connection.remoteAddress || this.req.socket.remoteAddress || this.req.connection.socket.remoteAddress;
-  const geo = "123" + ip;
-  next(null, {
-    geoip: {
-      country: geo ? geo : null
-    }
-  })
-}
